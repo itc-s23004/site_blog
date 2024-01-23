571c98915eb3c9a1c4a19c716a6a97eafa01677b
itc-s23004 <s23004@std.it-college.ac.jp>
2024-01-23 07:54:41 +0000

    2.6

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import Hero from 'components/hero'
-import Layout from 'components/layout'
 
 const Home = () => {
   return (
     <>
-      <Layout>
-        <Hero />
-      </Layout>
+      <Hero />
     </>
   )
 }
