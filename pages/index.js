5f13fe6889f03a795f597e6012a1741d3b851e1c
itc-s23004 <s23004@std.it-college.ac.jp>
2024-01-24 03:39:54 +0000

    2.9

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Hero from 'components/hero'
 const Home = () => {
   return (
     <>
-      <Hero title='CUBE' subtitle='アウトプットしていくサイト' />
+      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
     </>
   )
 }
