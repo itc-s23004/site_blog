ff58e0f5f2a4e160cf83c751916a5ae1c7a2966c
itc-s23004 <s23004@std.it-college.ac.jp>
2024-01-31 07:25:09 +0000

    7.6 fin

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,18 +1,26 @@
-import { client } from 'lib/api'
+import { getPostBySlug } from 'lib/api'
+import Container from 'components/container'
 
-const Schedule = () => {
-  const resPromise = client.get({
-    endpoint: 'blogs'
-  })
-  try {
-  const res = await resPromise
-  console.log(res)
-  } catch (err) {
-  console.log(err)
-    }
+const Schedule = ({ title, publish, content, eyecatch, categories }) => {
+  return (
+    <Container>
+      <h1>{title}</h1>
+    </Container>
+  )
+}
+export default Schedule
 
-  resPromise.then(res => console.log(res)).catch(err => console.log(err))
+const getStaticProps = async () => {
+  const slug = 'schedule'
+  const post = await getPostBySlug(slug)
   return {
-    props: {}
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories
+    }
   }
 }
+export { getStaticProps }
