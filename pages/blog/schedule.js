edfdf6fdfeed699177178d6485ec06736a883d00
itc-s23004 <s23004@std.it-college.ac.jp>
2024-02-01 05:34:58 +0000

    8.4

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,12 +1,24 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import Image from 'next/image'
 
 const Schedule = ({ title, publish, content, eyecatch, categories }) => {
   return (
     <Container>
       <article>
         <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+        <figuer>
+          <Image
+            src={eyecatch.url}
+            alt=''
+            layout='responsive'
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes='(min-width: 1125px) 1125px, 100vw'
+            priority
+          />
+        </figuer>
       </article>
       <h1>{title}</h1>
     </Container>
