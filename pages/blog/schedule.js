8abe5d7d26067fab51709c97ef27dd2f5fb2a135
itc-s23004 <s23004@std.it-college.ac.jp>
2024-02-06 06:46:36 +0000

    9.1

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,49 +1,59 @@
-import { getPostBySlug } from 'lib/api'
+import { getPostBySlug, getAllSlugs } from 'lib/api'
 import { extractText } from 'lib/extract-text'
+import { PrevNextPost } from 'lib/prev-next-post'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
 import PostBody from 'components/post-body'
+import Pagination from 'components/pagination'
 import {
   TwoColumn,
   TwoColumnMain,
   TwoColumnSidebar
 } from 'components/two-column'
-import PostCategories from 'components/post-categories'
 import ConvertBody from 'components/convert-body'
-import Image from 'next/image'
+import PostCategories from 'components/post-categories'
+import Image from 'next/legacy/image'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
-const Schedule = ({
+const Post = ({
   title,
   publish,
   content,
   eyecatch,
   categories,
-  description
+  description,
+  prevPost,
+  nextPost
 }) => {
   return (
     <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
       <article>
-        <Meta
-          pageTitle={title}
-          pageDesc={description}
-          pageImg={eyecatch.url}
-          pageImgW={eyecatch.width}
-          pageImgH={eyecatch.height}
-        />
-
         <PostHeader title={title} subtitle='Blog Article' publish={publish} />
-        <figuer>
+
+        <figure>
           <Image
+            key={eyecatch.url}
             src={eyecatch.url}
             alt=''
             layout='responsive'
             width={eyecatch.width}
             height={eyecatch.height}
-            sizes='(min-width: 1125px) 1125px, 100vw'
+            sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
           />
-        </figuer>
+        </figure>
+
         <TwoColumn>
           <TwoColumnMain>
             <PostBody>
@@ -54,25 +64,48 @@ const Schedule = ({
             <PostCategories categories={categories} />
           </TwoColumnSidebar>
         </TwoColumn>
+        <Pagination
+          prevText={prevPost.title}
+          prevUrl={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
       </article>
-      <h1>{title}</h1>
     </Container>
   )
 }
-export default Schedule
+export default Post
+const getStaticPaths = async () => {
+  const allSlugs = await getAllSlugs()
+  return {
+    paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
+    fallback: false
+  }
+}
+export { getStaticPaths }
 
-const getStaticProps = async () => {
-  const slug = 'schedule'
+const getStaticProps = async context => {
+  const slug = context.params.slug
   const post = await getPostBySlug(slug)
   const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
+
+  const allSlugs = await getAllSlugs()
+  const [prevPost, nextPost] = PrevNextPost(allSlugs, slug)
+
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch,
       categories: post.categories,
-      description
+      description,
+      prevPost,
+      nextPost
     }
   }
 }
